3d21930bbb1eaac14f96f1248653b834611dffbc
stein <steinadaaa@gmail.com>
2022-11-22 17:25:24 +0600

    delete time-work

--- Selenium/lesson6_step11.py
+++ Selenium/lesson6_step11.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
 try:
     link = 'http://suninjuly.github.io/registration1.html'
@@ -16,8 +15,6 @@ try:
 
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
-
-    time.sleep(1)
 
     welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
     welcome_text = welcome_text_elt.text
